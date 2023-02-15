280e482161aa083212b5789ad57d7c2c9f985e6f
Cyrilnkl <65498535+Cyrilnkl@users.noreply.github.com>
2023-02-15 15:36:50 +0100

    creation app

---
+++ TravelGo/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import First_page from './assets/screens/Auth/First_page';
+
+export default function App() {
+  return (
+    <First_page/>
+  );
+}
+
+const styles = StyleSheet.create({
+});
