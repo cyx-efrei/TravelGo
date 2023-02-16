8999ec2cbc70e078cce73c602ffbabdf9bd0a92b
Cyril Nakhla <cyrilnakhla@MacBook-Pro-de-Cyril.local>
2023-02-16 16:03:59 +0100

    ajout de commentaires

--- TravelGo/assets/config.js
+++ TravelGo/assets/config.js
@@ -1,3 +1,3 @@
-export const ActiveIconColor = 'red'
-export const InactiveIconColor = 'grey'
-export const iconSize = 30
+export const ActiveIconColor = 'red'        //couleur des boutons actifs
+export const InactiveIconColor = 'grey'     //couleur des boutons inactifs
+export const iconSize = 30                  //taille des incons
