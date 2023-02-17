fdf8df986a357807ba33f06d83939dcda2e917cb
Cyril Nakhla <cyrilnakhla@MacBook-Pro-de-Cyril.local>
2023-02-17 13:59:34 +0100

    Ajout de la barre de recherche pour la map

--- TravelGo/assets/screens/AppScreens/SearchScreen.js
+++ TravelGo/assets/screens/AppScreens/SearchScreen.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, TextInput, PixelRatio } from 'react-native'
 
 const SearchScreen = () => {
+
+  const [searchInput, setSearchInput] = useState('')
+
   return (
     <View style={styles.container}>
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Search"
+        placeholderTextColor="#ccc"
+        value={searchInput}
+        onChangeText={text => setSearchInput(text)}
+        />
       <MapView style={styles.map} />
     </View>
   );
@@ -15,9 +25,26 @@ export default SearchScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   map: {
     width: '100%',
     height: '100%',
   },
+  searchBar: {
+    position: 'absolute',
+    top: PixelRatio.getPixelSizeForLayoutSize(20),
+    width: '80%',
+    height: PixelRatio.getPixelSizeForLayoutSize(20),
+    paddingHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 2,
+    zIndex: 10,
+    backgroundColor: 'white',
+    color: 'green',
+    borderRadius: 20,
+    opacity: 0.9
+
+  }
 })
